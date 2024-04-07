459a91b861c0cd7a3ac962a812f05360377a07db
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-04-07 17:55:52 +0530

    Set Matrix Zero, Add Leetcode Solution 73, Brute Force Approach

---
+++ SetMatrixZeroes.java
@@ -0,0 +1,44 @@
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] row = new int[n];
+        int[] col = new int[m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i] == 1 || col[j] == 1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        SetMatrixZeroes obj = new SetMatrixZeroes();
+        obj.setZeroes(matrix);
+        System.out.println(obj);
+        // Print the modified matrix
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}

4e2cebebb1275bb6b5edfa9b14c4dd19cce3808f
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-04-10 21:42:40 +0530

    Shuffle_Array Class Create And Logic Implement and Display Output

--- ReverseArray.java
+++ ReverseArray.java
@@ -4,7 +4,6 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-
         int start = 0;
         int end = arr.length - 1;
 
@@ -19,6 +18,5 @@ public class ReverseArray {
         System.out.println(arr.length);
         System.out.println(arr[0]);
         System.out.println(arr[arr.length - 1]);
-
     }
 }
--- Shuffle_Array.java
+++ Shuffle_Array.java
@@ -1,16 +1,15 @@
 public class Shuffle_Array {
 
     public static void main(String[] args) {
-        int[] nums = {2,5,1,3,4,7};
+        int[] nums = {2, 5, 1, 3, 4, 7};
         int n = 3;
         int[] ans = new int[nums.length];
 
-        for(int i=0; i<nums.length; i++){
+        for(int i=0; i<n; i++){
             ans[i*2] = nums[i];
-            System.out.println(ans[i*2]);
-            ans[i*2+1] = nums[n+i];
+            ans[i*2+1] = nums[n+i]; // Issue occurs here
         }
-        for (int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(ans[i] + " ");
         }
 //        int j = 0;

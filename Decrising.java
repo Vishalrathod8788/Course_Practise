f976259f0de31854c2961317653296e65c9d37b9
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-04-10 21:43:12 +0530

    Remove Duplicate Array

---
+++ Decrising.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Decrising {
+    public static void main(String[] args) {
+//        for (int i = 10; i >= 1; i--) {
+//            System.out.println(i);
+//        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dec(n);
+    }
+
+    public static void dec(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+}

252c2aad4ca447595f40a6a26ff4cae772a36001
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-01 16:28:44 +0530

    Fectorial

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        myFact(n);
+    }
+    // Using Recursion
+
+    public static int myFact(int n){
+        if(n == 1){
+            return 1;
+        }
+        int fact = n * myFact(n-1);
+
+        System.out.println(fact);
+        return fact;
+
+
+
+    }
+}

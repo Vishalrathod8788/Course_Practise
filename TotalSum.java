f975402d9b1e93b5a170fcf1335991e181816397
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-01 17:24:29 +0530

    Total Number of Sum

---
+++ TotalSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+
+public class TotalSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = sumOfDigits(num);
+        System.out.println("Sum of digits: " + sum);
+        sc.close();
+    }
+
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n % 10 + sumOfDigits(n / 10);
+    }
+}

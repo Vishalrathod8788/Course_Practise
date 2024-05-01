6ea3bcba62802935d35e55894992283ad9ded2d7
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-01 16:29:13 +0530

    Fibbonacci Series

---
+++ Fibonnacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonnacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        fib(n);
+        for(int i=0;i<n;i++){
+            System.out.print(fib(i)+" ");
+        }
+    }
+    //write a function to print fibonnacci series using recursion
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}

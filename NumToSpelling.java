49d782e4fe451d3111c72eb13fda3801edb8a18a
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-01 16:48:28 +0530

    NumToSpelling Program

---
+++ NumToSpelling.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumToSpelling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        Spelling(Integer.parseInt(n));
+    }
+    public static void Spelling(int n){
+        String[] arr = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        if(n == 0){
+            return;
+        }
+        Spelling(n/10);
+        System.out.print(arr[n%10] + " ");
+    }
+}

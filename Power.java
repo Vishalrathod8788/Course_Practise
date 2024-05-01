d0db47608de97c5a17f857745a7b196b4be3f781
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-01 17:02:41 +0530

    any Power Of '2' Number

---
+++ Power.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int n = sc.nextInt();
+        MathPow(n);
+    }
+    public static int MathPow(int n){
+        if (n == 0)
+            return 1;
+        int res = 2 * MathPow(n-1);
+        System.out.println(res);
+        return res;
+    }
+}

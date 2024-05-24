050c0300030e244c43ff30e1b9de8565101f37f1
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-24 16:34:12 +0530

    Some Hashing Operation

--- com/vishal/hashing/Hashing.java
+++ com/vishal/hashing/Hashing.java
@@ -4,17 +4,16 @@ public class Hashing {
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
         hm.put("vishal", 1);
-        hm.put("vishal", 2);
+//        hm.put("vishal", 2);
         System.out.println(hm);
-        System.out.println(hm.get("vishal"));
-        System.out.println(hm.containsKey("vishal"));
+//        System.out.println(hm.get("vishal"));
+//        System.out.println(hm.containsKey("vishal"));
         System.out.println(hm.containsValue(1));
-        System.out.println(hm.size());
-        System.out.println(hm.isEmpty());
-        hm.remove("vishal");
-        System.out.println(hm);
-        System.out.println(hm.keySet());
-        System.out.println(hm.values());
-        System.out.println(hm.entrySet());
+//        System.out.println(hm.size());
+//        System.out.println(hm.isEmpty());
+//        hm.remove("vishal");
+//        System.out.println(hm);
+//        Set<String> s = hm.keySet();
+//        System.out.println(s);
     }
 }

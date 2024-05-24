418e3ea72f29a4cef41aa0010d6346d447b51fbf
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-24 16:40:57 +0530

    Some Hashing Operation

--- com/vishal/hashing/Hashing.java
+++ com/vishal/hashing/Hashing.java
@@ -4,16 +4,25 @@ public class Hashing {
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
         hm.put("vishal", 1);
-//        hm.put("vishal", 2);
+        hm.put("rathod", 2);
+        hm.put("VGR", 3);
+        hm.put("Coder", 4);
+        hm.put("Developer", 5);
         System.out.println(hm);
-//        System.out.println(hm.get("vishal"));
-//        System.out.println(hm.containsKey("vishal"));
+        System.out.println(hm.get("vishal"));
+        System.out.println(hm.containsKey("vishal"));
         System.out.println(hm.containsValue(1));
-//        System.out.println(hm.size());
-//        System.out.println(hm.isEmpty());
+        System.out.println(hm.size());
+        System.out.println(hm.isEmpty());
 //        hm.remove("vishal");
-//        System.out.println(hm);
+        System.out.println(hm);
 //        Set<String> s = hm.keySet();
-//        System.out.println(s);
+        for(String i : hm.keySet()){
+            System.out.println(i);
+        }
+        for (int i : hm.values()){
+            System.out.println(i);
+        }
+
     }
 }

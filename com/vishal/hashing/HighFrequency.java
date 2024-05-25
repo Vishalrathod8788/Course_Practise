27589327367de5f35fe49adf8753ddb61e921483
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-25 11:23:08 +0530

    Some Hashing Operation

---
+++ com/vishal/hashing/HighFrequency.java
@@ -0,0 +1,8 @@
+package com.vishal.hashing;
+
+public class HighFrequency {
+    public static void main(String[] args) {
+        String name = "Vishal";
+        System.out.println("Hey Mr. "+name);
+    }
+}

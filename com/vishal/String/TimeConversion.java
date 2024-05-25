8906e67027c59e30a593d3007974a258d10a8d8f
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-05-25 11:26:07 +0530

    Create New Package

---
+++ com/vishal/String/TimeConversion.java
@@ -0,0 +1,14 @@
+package com.vishal.String;
+
+public class TimeConversion {
+
+    static public String timeConversion(String s) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // Example usage:
+        System.out.println(timeConversion("07:45:54PM"));  // Output: "19:45:54"
+        System.out.println(timeConversion("12:01:00AM"));  // Output: "00:01:00"
+    }
+}

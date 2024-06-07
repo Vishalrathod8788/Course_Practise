3834417d6c504c6a81a234fbe70eaeefa15ef18c
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-07 19:53:30 +0530

    Some Code Write

--- com/vishal/stack/StackDemo.java
+++ com/vishal/stack/StackDemo.java
@@ -6,16 +6,11 @@ public class StackDemo {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
 
-        for(int i=1; i<5; i++){
+        for(int i=1; i<=5; i++){
             stack.push(i);
         }
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(5);
         System.out.println(stack);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < 5; i++) {
             System.out.println(stack+" "+stack.pop() +" "+ stack.size());
         }
     }

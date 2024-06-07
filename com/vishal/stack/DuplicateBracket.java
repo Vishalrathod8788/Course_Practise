ffb548cf92d2eb624314f5e70881c43184c4903a
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-07 20:08:19 +0530

    Some Code Write

--- com/vishal/stack/DuplicateBracket.java
+++ com/vishal/stack/DuplicateBracket.java
@@ -1,27 +1,30 @@
 package com.vishal.stack;
 
+import java.sql.SQLOutput;
 import java.util.Stack;
 
 public class DuplicateBracket {
     public static void main(String[] args) {
-        String  str = "(a+b) + (c+d)";
+        String str = "(a+b)+(c+d)";
+
         Stack<Character> st = new Stack<>();
-        for(int i=0; i<str.length(); i++){
+
+        for(int i=0; i < str.length(); i++){
             char ch = str.charAt(i);
             if(ch == ')'){
                 if(st.peek() == '('){
-                    System.out.println("Duplicate");
+                    System.out.println("Bracket Duplicate");
                     return;
                 }else{
-                    while (st.peek() != '('){
+                    while(st.peek() != '('){
                         st.pop();
                     }
-                    System.out.println("Not Duplicate");
-                    return;
+
                 }
             }else{
                 st.push(ch);
             }
         }
+        System.out.println("Non Duplicate Bracket");
     }
 }

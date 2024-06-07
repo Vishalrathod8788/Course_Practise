9a3318bdcd2b30190cdd00f12e5134bc86dc096f
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-07 19:13:51 +0530

    Function testing

--- com/vishal/stack/StackDemo.java
+++ com/vishal/stack/StackDemo.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 public class StackDemo {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
+
+        for(int i=1; i<5; i++){
+            stack.push(i);
+        }
         stack.push(1);
         stack.push(2);
         stack.push(3);

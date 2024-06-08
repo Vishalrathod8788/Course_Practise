3cb9e06c4c4f633649831c649274b054d0855b04
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-08 07:47:49 +0530

    Add Some New Code

--- com/vishal/stack/ValidParentheses.java
+++ com/vishal/stack/ValidParentheses.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class ValidParentheses {
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
         Stack<Character> st = new Stack<>();
 
         for(int i=0; i<str.length(); i++){
-
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char top = st.pop();
+                if (ch == ')' && top != '(' || ch == ']' && top != '[' || ch == '}' &&  top != '{'){
+                    return false;
+                }
+            }
         }
+        return st.isEmpty();
     }
 }

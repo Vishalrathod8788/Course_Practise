eb6c39d610ca589ea3080e2cc6801a31fd25b0fa
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-11 21:59:33 +0530

    Create AddNode Class

---
+++ com/vishal/LikedList/InsertNode.java
@@ -0,0 +1,18 @@
+package com.vishal.LikedList;
+
+public class InsertNode {
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+
+        }
+    }
+}

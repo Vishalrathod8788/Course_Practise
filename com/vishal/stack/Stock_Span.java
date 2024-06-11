b23049a3ea3fc2bcd3c829bbe20ef4a7d89fbd41
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-11 16:25:30 +0530

    Create Stock_Span class

---
+++ com/vishal/stack/Stock_Span.java
@@ -0,0 +1,14 @@
+package com.vishal.stack;
+
+import java.util.Stack;
+
+public class Stock_Span {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        int[] arr = {100,80,60,70,60,75,85};
+        int[] ans = new int[arr.length];
+        st.push(0);
+        ans[0] = 1;
+
+    }
+}

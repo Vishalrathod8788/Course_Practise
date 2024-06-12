4690b90e7b83c5f3413995fd9233ffb96bed6b2d
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 19:02:55 +0530

    Main Method Add In class

--- com/vishal/LikedList/AddFirst.java
+++ com/vishal/LikedList/AddFirst.java
@@ -2,5 +2,34 @@ package com.vishal.LikedList;
 
 public class AddFirst {
 
+    public static class Node{
+        int data;
+        Node next;
+    }
+    public static class Linked_List{
+        Node head;
+        Node tail;
+        int size;
 
+        void Insertion(int val){
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if(size == 0){
+                head = tail = temp;
+            }else{
+                tail = tail.next;
+                tail = temp;
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Linked_List ls = new Linked_List();
+
+        ls.Insertion(10);
+    }
 }

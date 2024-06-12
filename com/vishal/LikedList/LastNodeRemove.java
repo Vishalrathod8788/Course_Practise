f0607686ed4ea5925d8e41a1a5d309194395737d
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 18:26:10 +0530

    Some Code Change

--- com/vishal/LikedList/LastNodeRemove.java
+++ com/vishal/LikedList/LastNodeRemove.java
@@ -34,20 +34,17 @@ public class LastNodeRemove {
         }
 
         public void RemoveLastNode(){
-            if(size==0){
-                System.out.println("List is empty");
-            }
-            else if(size==1){
-                head=tail=null;
-                size=0;
-            }
-            else{
-                Node temp=head;
-                for(int i=0;i<size-2;i++){
-                    temp=temp.next;
+            if(size == 0){
+                System.out.println("List is Empty :");
+            } else if (size == 1) {
+                head = tail = null;
+            }else {
+                Node temp = head;
+                for (int i=0; i<size-2; i++){
+                    temp = temp.next;
                 }
-                temp.next=null;
-                tail=temp;
+                temp.next = null;
+                tail = temp;
                 size--;
             }
         }
@@ -68,6 +65,9 @@ public class LastNodeRemove {
         ls.Insert(90);
         ls.Insert(100);
 
+        ls.Display();
+        ls.RemoveLastNode();
+        System.out.println();
         ls.Display();
         ls.RemoveLastNode();
         System.out.println();

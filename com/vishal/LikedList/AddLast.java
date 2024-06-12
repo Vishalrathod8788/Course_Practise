8605fb8177bbe7129f105972edf685521c534dde
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 19:38:51 +0530

    Create AddLast Class

---
+++ com/vishal/LikedList/AddLast.java
@@ -0,0 +1,18 @@
+package com.vishal.LikedList;
+
+public class AddLast {
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class LinkList{
+        Node head;
+        Node tail;
+        int size;
+
+        void AddLast(int val){
+
+        }
+    }
+}

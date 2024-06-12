05a08225d06d62244f5918ceee2f1e48d8e1e310
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 19:13:14 +0530

    AddFirst class in some code change

--- com/vishal/LikedList/AddFirst.java
+++ com/vishal/LikedList/AddFirst.java
@@ -2,34 +2,41 @@ package com.vishal.LikedList;
 
 public class AddFirst {
 
-    public static class Node{
-        int data;
-        Node next;
-    }
-    public static class Linked_List{
-        Node head;
-        Node tail;
-        int size;
-
-        void Insertion(int val){
-            Node temp = new Node();
-            temp.data = val;
-            temp.next = null;
-
-            if(size == 0){
-                head = tail = temp;
-            }else{
-                tail = tail.next;
-                tail = temp;
-            }
-        }
-    }
+//    public static class Node{
+//        int data;
+//        Node next;
+//    }
+//    public static class Linked_List{
+//        Node head;
+//        Node tail;
+//        int size;
+//
+//        void Insertion(int val){
+//            Node temp = new Node();
+//            temp.data = val;
+//            temp.next = null;
+//
+//            if(size == 0){
+//                head = tail = temp;
+//            }else{
+//                tail = tail.next;
+//                tail = temp;
+//            }
+//        }
+//    }
 
 
 
     public static void main(String[] args) {
-        Linked_List ls = new Linked_List();
+        InsertNode.LikedList add = new InsertNode.LikedList();
+        DisplayNode.Link display = new DisplayNode.Link();
+        add.addNode(100);
+        add.addNode(200);
+        add.addNode(300);
+        add.addNode(400);
+        add.addNode(500);
 
-        ls.Insertion(10);
+        display.Display();
+
     }
 }

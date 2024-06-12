adb7f144086f3f2ebe1d25fc92342f0e3a0ad915
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 09:57:56 +0530

    Some Code Add

--- com/vishal/LikedList/InsertNode.java
+++ com/vishal/LikedList/InsertNode.java
@@ -2,10 +2,17 @@ package com.vishal.LikedList;
 
 public class InsertNode {
     public static class Node{
-
+        int data;
+        Node next;
     }
     public static class LikedList{
+        Node head;
+        Node tail;
+        int size;
 
+        void addNode(int val){
+
+        }
     }
 
     public static void main(String args[]){

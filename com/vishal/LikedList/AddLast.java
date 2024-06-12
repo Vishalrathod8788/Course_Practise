ed7ac8772089be44c33e105b7e0df8769f875bc9
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 19:51:54 +0530

    Some Code Add

--- com/vishal/LikedList/AddLast.java
+++ com/vishal/LikedList/AddLast.java
@@ -12,7 +12,35 @@ public class AddLast {
         int size;
 
         void AddLast(int val){
-
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+            if(size == 0){
+                head = tail = temp;
+            }else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
         }
+        void Display(){
+            Node temp = head;
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkList ls = new LinkList();
+
+        ls.Display();
+        ls.AddLast(500);
+        ls.AddLast(600);
+        ls.AddLast(700);
+        ls.Display();
+        ls.AddLast(1000);
+        ls.Display();
     }
 }

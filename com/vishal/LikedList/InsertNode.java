8ceceaf54b709d4597ead4e7cc366f3f3c54e28d
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 11:36:28 +0530

    Some Code Change

--- com/vishal/LikedList/InsertNode.java
+++ com/vishal/LikedList/InsertNode.java
@@ -11,24 +11,25 @@ public class InsertNode {
         int size;
 
         void addNode(int val){
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
             if (size == 0){
-                Node temp = new Node();
-                temp.data = val;
-                temp.next = null;
-                head = tail = null;
-                size++;
+                head = tail = temp;
             }else {
-                Node temp = new Node();
-                temp.data = val;
-                temp.next = null;
                 tail.next = temp;
                 tail = temp;
             }
+            size++;
         }
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         LikedList ls = new LikedList();
-
+        ls.addNode(10);
+        ls.addNode(20);
+        ls.addNode(30);
+        ls.addNode(40);
+        ls.addNode(50);
     }
 }

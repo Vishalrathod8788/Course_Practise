06361dfbc2f71cd3fcd56e4edd7a405198a2b63b
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-12 09:46:22 +0530

    Some Code Add

--- com/vishal/LikedList/InsertNode.java
+++ com/vishal/LikedList/InsertNode.java
@@ -6,48 +6,13 @@ public class InsertNode {
         Node next;
     }
 
-    public static class LinkedList {
+    public static class LinkedList{
         Node head;
         Node tail;
         int size;
-
-        void addLast(int val) {
-            if(size == 0){
-                Node temp = new Node();
-                temp.data = val;
-                temp.next = null;
-                head = tail = temp;
-                size++;
-            }else{
-                Node temp = new Node();
-                temp.data = val;
-                temp.next = null;
-                tail.next = temp;
-                tail = temp;
-                size++;
-            }
-        }
-        void display(){
-            Node temp = head;
-            while(temp != null){
-                System.out.print(temp.data+" ");
-                temp = temp.next;
-            }
-        }
     }
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(1);
-        list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
-        list.addLast(5);
-        list.addLast(6);
-        list.addLast(7);
-        list.addLast(8);
-        list.addLast(9);
-        list.addLast(10);
 
-        list.display();
+    public static void addNode(int val){
+
     }
 }

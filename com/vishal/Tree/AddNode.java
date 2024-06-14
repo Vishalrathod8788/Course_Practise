eae24e09745ba6eca59759d8ec1f823ef1f22d01
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-14 19:19:26 +0530

    Node Testing

--- com/vishal/Tree/AddNode.java
+++ com/vishal/Tree/AddNode.java
@@ -12,4 +12,27 @@ public class AddNode {
             this.right = right;
         }
     }
+
+    public static class pair{
+        Node node;
+        int state;
+
+        pair(Node node, int state){
+            this.node = node;
+            this.state = state;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, 40, null, null,
+                75, 62, 60, null, null, 70, null, null, 87, null, null};
+
+        Node root = new Node(arr[0], null, null);
+        pair rootPair = new pair(root, 1);
+        int idx = 1;
+
+    }
+
 }

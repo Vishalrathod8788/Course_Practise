eb239db6945a026f72184f9ce367676842a86b1a
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-20 22:41:03 +0530

    Some Code Add

--- com/vishal/Queue/AddData.java
+++ com/vishal/Queue/AddData.java
@@ -1,6 +1,34 @@
 package com.vishal.Queue;
 public class AddData {
-    public static void main(String[] args) {
-        System.out.println("Hey Vishal Ratod");
+
+    public static class Queue{
+
+        static int[] arr;
+        static int size;
+        static int rear;
+
+        Queue(int n){
+            int[] arr = new int[n];
+            int size = n;
+            int rear = -1;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        public static int[] Add(int data){
+            if(rear == size - 1){
+                return arr;
+            }else {
+                rear++;
+                arr[rear] = data;
+            }
+            return arr;
+        }
+
+        public static void remove(){
+
+        }
     }
 }

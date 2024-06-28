5a5b34f20cf0f6f1ac55a88b984d850972c35cba
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-28 11:33:06 +0530

    Sum Method Implement

--- com/vishal/Tree/SumSize.java
+++ com/vishal/Tree/SumSize.java
@@ -1,5 +1,43 @@
 package com.vishal.Tree;
 
 public class SumSize {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
 
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static int sum(Node node){
+        if(node == null){
+            return 0;
+        }
+
+        int ls = sum(node.left);
+        int rs = sum(node.right);
+        int ts = ls + rs + node.data;
+
+        return ts;
+    }
+
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int sum = sum(root);
+        System.out.println("Sum of all nodes: " + sum);
+
+        int total = size(root);
+        System.out.println("Size of tree: " + total);
+    }
 }

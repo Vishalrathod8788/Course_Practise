3d020740c0c338561cd69ae01ea52a3f7fae1647
Vishalrathod8788 <vishalrathod1562004@gmail.com>
2024-06-28 11:33:22 +0530

    Size Method Implement

--- com/vishal/Tree/SumSize.java
+++ com/vishal/Tree/SumSize.java
@@ -24,6 +24,18 @@ public class SumSize {
         return ts;
     }
 
+    public static int size(Node node){
+        if(node == null){
+            return 0;
+        }
+        int ls = size(node.left);
+        int rs = size(node.right);
+        int ts = ls + rs + 1;
+
+        return ts;
+    }
+
+
 
     public static void main(String[] args) {
         Node root = new Node(1);

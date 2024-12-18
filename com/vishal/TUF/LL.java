7aa93077a894436a78b6d4ce90569fe078103fd1
Vishal Rathod <vishalrathod1562004@gmail.com>
2024-12-18 16:01:13 +0530

    Add new Data

--- com/vishal/TUF/LL.java
+++ com/vishal/TUF/LL.java
@@ -1,4 +1,45 @@
 package com.vishal.TUF;
 
 public class LL {
+    //create node class
+    static class Node{
+        int data;
+        Node next;
+
+        //constructor (insert Ele)
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+        //constructor (insert Head)
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+
+        //Default constructor create (Default Constructor)
+        public Node(){
+            this.data = 0;
+            this.next = null;
+        }
+
+        //constructor
+        public Node(Node head, int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public static Node  insertHead(Node head, int data) {
+        Node temp = new Node(data, head);
+        head = temp;
+        return head;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        Node head = new Node(arr[0], null);
+        head = insertHead(head, arr[0]);
+        System.out.println(head.data);
+
+    }
 }
--- com/vishal/TUF/main.java
+++ com/vishal/TUF/main.java
@@ -1,4 +1,34 @@
 package com.vishal.TUF;
 
 public class main {
+    public static class Node{
+        int data;
+        Node next;
+    }
+
+    public static class LinkedLiost{
+        Node head;
+        Node tail;
+        int size;
+
+        public void InsertNode(int x){
+            Node temp = new Node();
+            temp.data = x;
+            temp.next = null;
+
+            if(size < 0){
+                head = tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public static void main(String[] args) {
+            LinkedLiost list = new LinkedLiost();
+            list.InsertNode(10);
+            list.InsertNode(20);
+        }
+    }
 }
